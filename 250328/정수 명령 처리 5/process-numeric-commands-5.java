d36f9fc5b9ebc78a601136f9b94a9742c2d192d7
Seohyeon.Lim <132811149+nunLSH@users.noreply.github.com>
2025-03-28 18:37:58 +0900

    Title: 정수 명령 처리 5, Time: 284ms, Memory: 17MB, Status: Passed - Codetree

---
+++ 250328/정수 명령 처리 5/process-numeric-commands-5.java
@@ -0,0 +1,28 @@
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        ArrayList<Integer> intArr = new ArrayList();
+
+        int n = sc.nextInt();
+
+        for (int i = 0; i < n; i++){
+            String command = sc.next();
+
+            if (command.equals("push_back")){
+                int a = sc.nextInt();
+                intArr.add(a);
+            } else if (command.equals("pop_back")){
+                int index = intArr.size() - 1;
+                intArr.remove(index);
+            } else if (command.equals("size")){
+                System.out.println(intArr.size());
+            } else {
+                int k = sc.nextInt();
+                System.out.println(intArr.get(k - 1));
+            }
+        }
+    }
+}
